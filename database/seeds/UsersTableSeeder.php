1e88b39ab8213b8bedc5684d61689dfde7ea43ce
ue8d <webmaster@tomoki12.com>
2020-12-02 01:46:53 +0900

    ユーザーデータ登録用のSeedを追加

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'name'           => 'TEST' .$i,
+                'email'          => 'test' .$i .'@test.com',
+                'password'       => Hash::make('12345678'),
+                'remember_token' => str_random(10),
+                'created_at'     => now(),
+                'updated_at'     => now()
+            ]);
+        }
+    }
+}
