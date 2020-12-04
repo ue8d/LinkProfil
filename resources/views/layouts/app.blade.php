c9ffb0dd8bba9180dc2f32ea580f24462e44ee9a
ue8d <webmaster@tomoki12.com>
2020-12-04 21:01:07 +0900

    ナビの編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,11 +50,20 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->screen_name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/users/'. Auth::id()) }}">
+                                        マイページ
+                                    </a>
+
+                                    <a class="dropdown-item" href="{{ url('/users') }}">
+                                        ユーザ一覧
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
