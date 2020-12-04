038e0141911e7d6d1b26f688692f78d692175ff3
ue8d <webmaster@tomoki12.com>
2020-12-04 21:00:48 +0900

    Toppageの編集

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>LinkProfil</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -68,7 +68,8 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/users') }}">Home</a>
+                        <a href="{{ url('/users/'. Auth::id() .'/edit') }}">Profil edit</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -81,18 +82,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LinkProfil
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://ue8d.com/">ue8d.com</a>
+                    <a href="https://tomoki.vip">tomoki.vip</a>
+                    <a href="https://tomoki12.com">tomoki12.com</a>
+                    <a href="https://github.com/ue8d/">GitHub</a>
                 </div>
             </div>
         </div>
