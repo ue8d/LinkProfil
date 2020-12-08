38f3028a8ad279dfcbec018bd7da0a75fa12e8f2
ue8d <webmaster@tomoki12.com>
2020-12-08 18:14:52 +0900

    タイトルを環境変数から取得するように変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LinkProfil</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LinkProfil
+                    {{ config('app.name', 'Laravel') }}
                 </div>
 
                 <div class="links">
