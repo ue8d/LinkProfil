c5073c87168c90ec7268d555bdf8134d89ce7c65
ue8d <webmaster@tomoki12.com>
2020-12-12 00:25:32 +0900

    ヴァリデーションの変更

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -52,8 +52,8 @@ class UsersController extends Controller
     {
         $data = $request->all();
         $validator = Validator::make($data, [
-            'screen_name'   => ['required', 'string', 'max:50', Rule::unique('users')->ignore($user->id)],
-            'name'          => ['required', 'string', new Hankaku, 'max:255'],
+            'screen_name'   => ['required', 'string', 'max:50'],
+            'name'          => ['required', 'string', new Hankaku, 'max:255', Rule::unique('users')->ignore($user->id)],
             'profile_image' => ['file', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
             'email'         => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)]
         ]);
